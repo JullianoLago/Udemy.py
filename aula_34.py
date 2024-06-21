864315d7f47cfc4e71a63dda5db445841156d061
Julliano Lago <jullianolago10@gmail.com>
2024-06-21 08:32:54 -0300

    first commit

---
+++ aula_34.py
@@ -0,0 +1,13 @@
+# if / elif       / else
+# se / se não se  / se não 
+
+Genero = input("Você é homem ou mulher ? ")
+if Genero == "homem":
+    print("Então vc é do sexo masculino.")
+
+elif Genero == "mulher":
+    print("Então vc é do sexo feminino.")
+
+else:
+    print("Você não é homem nem mulher.")
+
